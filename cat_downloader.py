ce7f62a3e99f158f90fb3a0304beaa8149c81725
omerhac <omerhac94@gmail.com>
2020-10-31 02:20:12 +0200

    Finished cat_downloader

--- cat_downloader.py
+++ cat_downloader.py
@@ -1,6 +1,8 @@
 import json
 import subprocess
 import requests
+import time
+import os
 
 
 def authorize(client):
@@ -69,17 +71,79 @@ def get_cat_images(cat):
     else:
         jpeg_photos = []
         for photo_url in medium_photos:
-            img = requests.get(photo_url).content
-            jpeg_photos.append(img)
-        return jpeg_photos
+            try:
+                img = requests.get(photo_url).content
+                jpeg_photos.append(img)
+            except:
+                pass
+
+        # check number of images again
+        if len(jpeg_photos) < 5:
+            return []
+        else:
+            return jpeg_photos
 
 
-def download_cats():
-    pass
+def download_cats(n_pages=5):
+    """Download n_pages of cats"""
+    # authorize
+    client = 1  # start with first client
+    authorize(client)
 
+    count = 0
+    # document every download to index.txt
+    for page in range(1, n_pages):
+        request = f'animals?type=cat&page={page}'
+        response, response_code = petfinder_request(request, client)
 
+        # check response code
+        while response_code == 429:  # rate limit hit and we should wait
+            print("Rate limit hit, switching client and waiting 5 minutes")
+            time.sleep(300)
+            client = 1 if client == 2 else 2
+            response, response_code = petfinder_request(request, client, new_token=True)
 
+        while response_code == 500:  # internal error
+            print("Internal error, waiting 1 minute")
+            time.sleep(60)
+            response, response_code = petfinder_request(request, client)
+
+        if response_code != 200:
+            print(f"Error {response_code} in the request. Response text is {response}")
+            return
+
+        # get cats
+        cats = response['animals']
+
+        # iterate every cat
+        for cat in cats:
+            # create cat path
+            cat_id = cat['id']
+            cat_lib = f'images/{cat_id}/raw'
+            if os.path.isdir(cat_lib):
+                print("Skipping cat {}, already exists..".format(cat_id))
+
+            cat_images = get_cat_images(cat)
+            if len(cat_images) > 0:
+                print(f"Wrting cat {cat_id}...")
+                os.makedirs(cat_lib, exist_ok=True)
+
+                # save images
+                for i, image in enumerate(cat_images):
+                    with open(cat_lib + f'/{i}.jpg', 'wb') as file:
+                        file.write(image)
+
+                # document in index
+                with open('index.txt', 'w') as index:
+                    index.write(f'Wrote cat {cat_id} from page {page} with {len(cat_images)} images \n')
+                    
+            else:
+                print(f"Cat {cat_id} has not enough images")
+
+            count += 1
+
+    print(f"Finished {count} cats")
 
 
 if __name__ == '__main__':
-    print(petfinder_request('animals?type=cat', 1))
+    download_cats()
