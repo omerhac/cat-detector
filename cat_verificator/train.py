124c9fcf78e7fef4719820d82b2b5e3f2e36922f
omerhac <omerhac94@gmail.com>
2020-12-08 18:51:36 +0200

    train should now get a batched and repeated datset. dataset size also

--- cat_verificator/train.py
+++ cat_verificator/train.py
@@ -2,6 +2,7 @@ from modules import *
 from triplet_loss import _get_anchor_negative_triplet_mask, batch_all_triplet_loss, batch_hard_triplet_loss
 import etl
 import os
+import glob
 
 # constants
 MARGIN = 0.1
@@ -39,34 +40,50 @@ def loss_function(labels, embeddings, alpha):
     return l_triplet + alpha * l_gor
 
 
-def train_stage(model, dataset, optimizer, epcohs=30):
-    """One training stage in the training process"""
-    for epoch in range(epcohs):
+def train_stage(model, dataset, optimizer, dataset_size, batch_size, epochs=30):
+    """One training stage in the training process.
+    Args:
+        model: model to train
+        dataset: cat images dataset. batched and repeated.
+        optimizer: initialized training optimizer
+        dataset_size: number of images in the dataset
+        batch_size: images batch size
+        epochs: number of epochs
+    """
+    for epoch in range(epochs):
         print(f'Epoch number {epoch}')
 
         for batch_num, (batch_labels, batch_images) in enumerate(dataset):
             # calculate and apply gradients
             with tf.GradientTape() as tape:
                 batch_embeddings = model(batch_images)
-                loss, _ = batch_all_triplet_loss(batch_labels, batch_embeddings, MARGIN)
+                loss = loss_function(batch_labels, batch_embeddings, MARGIN)  # compute loss
 
-                # apply
+                # apply graduebts
                 grads_and_vars = tape.gradient(loss, model.trainable_variables)
                 optimizer.apply_gradients(zip(grads_and_vars, model.trainable_variables))
             # print loss
             if batch_num % 1 == 0:  # TODO: change to 50
-                print(f'Current loss is {loss.numpy()}')
+                print(f'Batch {batch_num}, Current loss is {loss.numpy()}')
+
+            # finished dataset break rule
+            if batch_num == dataset_size // batch_size - 1:
+                break
 
 
 def train():
     """Train cat verificator"""
-    # initiate
+    # initiate a model
     model = CatVerificator(input_shape=(64, 64, 3))
-    images_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    dataset = etl.image_generator(images_dir, image_size=(64, 64)).batch(10)
+
+    # get dataset
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/images'
+    dataset, dataset_size = etl.image_generator(base_dir, image_size=(64, 64))
+    dataset = dataset.repeat()
+    dataset = dataset.batch(10)
     optimizer = tf.keras.optimizers.Adam()
 
-    train_stage(model, dataset, optimizer)
+    train_stage(model, dataset, optimizer, dataset_size, 10)
 
 
 if __name__ == '__main__':
@@ -79,3 +96,4 @@ if __name__ == '__main__':
         0, 1, 1
     ])
     print(loss_function(b, a, 0.5))
+    train()
