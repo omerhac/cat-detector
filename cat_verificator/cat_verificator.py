4891e2ac94ad221c0b6e57573c84bca7da36a527
omerhac <omerhac94@gmail.com>
2020-12-13 00:44:03 +0200

    added catverificator object to difrrantiate between cats

---
+++ cat_verificator/cat_verificator.py
@@ -0,0 +1,38 @@
+from modules import *
+import tensorflow as tf
+from etl import resize_image
+
+
+class CatVerificator():
+    """Cat verificator object
+    Attributes:
+        cat_embedder: CatEmbedder model to embed cat images
+        threshold: threshold for separating different and same cats
+
+    Methods:
+         set_threhold: set separating threshold
+         is_same_cat: check whether to images are of the same cat
+    """
+
+    def __init__(self, cat_embedder, threshold):
+        """Initialize a cat verficator.
+        Args:
+            cat_embedder: CatEmbedder model
+            threshold: seperating threshold
+        """
+
+        self._cat_embedder = cat_embedder
+        self._threshold = threshold
+
+    def set_threshold(self, threshold):
+        """Set new threshold to threshold"""
+        self._threshold = threshold
+
+    def is_same_cat(self, cat_1, cat_2):
+        """Check whether cat_1 and cat_2 are images of the same cat. Resize image if necessary"""
+        # get cat embedder input size
+        image_shape = self._cat_embedder.get_input_shape()
+        image_height, image_width = image_shape[0], image_shape[2]
+
+        cat1_embed = self._cat_embedder(resize_image(cat_1, height=image_height, width=image_width))
+        cat2_embed = self._cat_embedder(resize_image(cat_2, height=image_height, width=image_width))
