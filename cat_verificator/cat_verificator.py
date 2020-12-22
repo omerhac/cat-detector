93c0be9e03fe4713a9e05b80c1565686459c220f
omerhac <omerhac94@gmail.com>
2020-12-23 01:05:49 +0200

    problem with save mechanism

--- cat_verificator/cat_verificator.py
+++ cat_verificator/cat_verificator.py
@@ -1,6 +1,7 @@
 from modules import *
 import tensorflow as tf
-from etl import resize_image, read_image
+from etl import resize_image
+
 import os
 import matplotlib.pyplot as plt
 import utilities
@@ -18,21 +19,27 @@ tf.compat.v1.disable_eager_execution()
 class CatVerificator():
     """Cat verificator object
     Attributes:
-        cat_embedder: CatEmbedder model to embed cat images
-        threshold: threshold for separating different and same cats
-        data_dir: directory to store application data. structure:
+        _cat_embedder: CatEmbedder model to embed cat images
+        _threshold: threshold for separating different and same cats
+        _data_dir: directory to store application data. structure:
             - images
                 - cropped: cropped images
                 - raw: raw images
             - own_embedding.dat: pickled own embedding
             - threshold.txt: separating threshold
+        _own_embedding: own cat embedding
+        _own_embedding_ph: placeholder for own embedding
+        _image_to_verify: placeholder for verification graph
+        _verification_graph: tf verifcation graph
+        _sess: tf session
 
     Methods:
          set_threhold: set separating threshold
-         is_same_cat: check whether 2 images are of the same cat
          is_own_cat: check whether a cat is own cat
          set_own_image: sets the own cat image of the application
          resize_input: resizes input images for CatEmbedder digestion
+         create_verification_grpah: create tf verification graph
+         close_session: close tf session
     """
 
     def __init__(self, embedder_input_shape, threshold=1.25, data_dir='data', load_data=False):
@@ -50,7 +57,7 @@ class CatVerificator():
         # set attrs
         self._threshold = threshold
         self._data_dir = data_dir
-        self._own_embedding = tf.zeros(shape=[1, 64], dtype=tf.float32)
+        self._own_embedding_ph = tf.compat.v1.placeholder(tf.float32, shape=[1, 64])
         self._image_to_verify = tf.compat.v1.placeholder(tf.float32, shape=[None, None, 3])
         self._verification_graph = self.create_verification_graph()
         self._sess = K.get_session()
@@ -58,14 +65,14 @@ class CatVerificator():
         # load data if required
         if load_data:
             self._threshold = float(open(data_dir + '/threshold.txt', 'r').readline())
-            self._own_embedding = tf.constant(pickle.load(open(data_dir + '/own_embedding.dat', 'rb')),
-                                              dtype=tf.float32)
+            self._own_embedding = np.array(pickle.load(open(data_dir + '/own_embedding.dat', 'rb')))
 
         # save data
         else:
             if not os.path.exists(data_dir):
                 os.makedirs(data_dir)
             open(data_dir + '/threshold.txt', 'w').write(str(self._threshold))
+            self._own_embedding = None
 
     def close_session(self):
         """Close current verification session"""
@@ -88,17 +95,21 @@ class CatVerificator():
         cat_embedd = self._cat_embedder([resized_cat])
 
         # get distance
-        distance = tf.reduce_sum(tf.pow(cat_embedd - self._own_embedding, 2))
-        return distance < self._threshold, cat_embedd
+        distance = tf.reduce_sum(tf.pow(cat_embedd - self._own_embedding_ph, 2))
+        return distance < self._threshold, distance
 
     def is_own_cat(self, cat):
         """Check whether cat and own cat are the same cat. Resize image if necessary.
         Args:
             cat: cropped image of a face of a cat. array/tensor
         """
+        assert self._own_embedding is not None, "No own cat chosen yet"
 
         # run verification graph
-        return self._sess.run(self._verification_graph, feed_dict={self._image_to_verify: cat})
+        return self._sess.run(self._verification_graph, feed_dict={
+            self._image_to_verify: cat,
+            self._own_embedding_ph: self._own_embedding
+        })
 
     def set_own_image(self, image):
         """Set own cat image to be image. Also embedds the image and save it as _own_embedding.
@@ -135,13 +146,12 @@ class CatVerificator():
 
 if __name__ == '__main__':
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/images'
-    path1 = base_dir + '/49403512/cropped/1.jpg'
-    path2 = base_dir + '/49726525/cropped/1.jpg'
+    path1 = base_dir + '/49403512/raw/2.jpg'
+    path2 = base_dir + '/49403512/cropped/4.jpg'
     cat1 = plt.imread(path1)
     cat2 = plt.imread(path2)
 
-    cat_ver = CatVerificator([64, 64, 3], 1.25, 'data', load_data=True)
+    cat_ver = CatVerificator([64, 64, 3], 1.1, 'data', load_data=True)
     #cat_ver.set_own_image(cat1)
     print(cat_ver.is_own_cat(cat2))
-    print(pickle.load(open('data/own_embedding.dat', 'rb')))
 
