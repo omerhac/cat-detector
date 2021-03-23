e72e289ca630555591b3b0a2c61a6c8cf62296cd
omerhac <omerhac94@gmail.com>
2021-03-23 12:52:24 +0200

    modified sagemaker docker file to include aws cli installation and authentication

--- aws_credentials/authenticate.sh
+++
@@ -1,7 +0,0 @@
-# get aws configuration paths
-aws_credentials_path=../.aws/credentials
-aws_config_path=../.aws/config
-
-# copy credentials to container aws cli configuration paths
-cp credentials $aws_credentials_path
-cp config $aws_config_path
